b96bf2b911b0623ebe9cc70e337fce08f3f059fb
MishMash95 <Michaelpw11@hotmail.com>
2015-11-07 17:06:22 +0000

    Updated main file

--- dungeongame/src/com/dungeoncrawler/game/Crawler.java
+++ dungeongame/src/com/dungeoncrawler/game/Crawler.java
@@ -1,8 +1,23 @@
 package com.dungeoncrawler.game;
 
+import com.dungeoncrawler.game.render.DungeonScene;
+
+import Application3D.Application3D;
+
 public class Crawler {
+	public static Application3D app3D;
 	public static void main(String[] args) {
+		
+		
+		
 		// TODO Auto-generated method stub
+		app3D = Application3D.getApp();
+		app3D.start();
+		
+		DungeonScene ds = new DungeonScene();
+		app3D.registerRenderInstance(ds);
+		
 		System.out.println("test");
+		
 	}
 }
