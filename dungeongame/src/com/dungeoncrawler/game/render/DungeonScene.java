7a1ff98c13b4084baa20dc16da8532d40785d6b6
MishMash95 <Michaelpw11@hotmail.com>
2015-11-08 07:17:42 +0000

    Added stuff and things

--- dungeongame/src/com/dungeoncrawler/game/render/DungeonScene.java
+++ dungeongame/src/com/dungeoncrawler/game/render/DungeonScene.java
@@ -8,6 +8,10 @@ import utils3D.RenderUtils;
 import utils3D.Texture;
 import utils3D.VertexBuffer;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
@@ -16,19 +20,35 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 
+import com.dungeoncrawler.game.entity.Entity;
+import com.dungeoncrawler.game.entity.EntityCardPic;
+import com.dungeoncrawler.generator.Dungeon;
+import com.dungeoncrawler.generator.GeneratorOptions;
 import com.dungeoncrawler.generator.Tile;
+import com.dungeoncrawler.generator.search.IHeuristic;
+import com.dungeoncrawler.generator.search.PriorityQueue;
+import com.sun.media.sound.ModelChannelMixer;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 public class DungeonScene implements Renderer3D {
 
 	private Application3D app3D = Application3D.getApp();
-	private Texture texFloor, texWall, texDungeon;
-	private VertexBuffer sceneFloors, sceneWalls, archModel, wallModel, floorModel, ceilingModel;
+	private Texture texFloor, texWall, texDungeon, texDetail;
+	private VertexBuffer sceneFloors, sceneWalls, sceneDetails, archModel, wallModel, floorModel, ceilingModel, candelabraModel, torch1Model;
 	
 	private Tile[][] map;
 	private int mapSize = 30;
 	private int scale   = 32; // Size of generated geometry per cell in units.
 	private float playerPadding = 10.0f;
 	
+	private ArrayList<PointLight> lights;
+	private ArrayList<Entity> entities;
+	private PointLight playerLight;
+	private Random generator;
+	
+	float camWobbleBoyes;
+	
+	boolean placed = false;
 	
 	@Override
 	public void init() {
@@ -36,6 +56,10 @@ public class DungeonScene implements Renderer3D {
 		System.out.println("Creating Dungeon Renderer");
 		GL11.glEnable(GL11.GL_CULL_FACE);
 		GL11.glCullFace(GL11.GL_FRONT);
+		
+		generator = new Random();
+		lights    = new ArrayList<>();
+		entities  = new ArrayList<>();
 		
 		// Setup rendering properties
 		/*GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_)
@@ -48,7 +72,10 @@ public class DungeonScene implements Renderer3D {
 		
 		// *********************************************** //
 		// Create map
-		map = new Tile[mapSize][mapSize];
+		Dungeon d = new Dungeon(new GeneratorOptions());
+		map = d.tiles;
+		mapSize = map.length;
+		/*map = new Tile[mapSize][mapSize];
 		for( int i=0; i < mapSize; i++ ) {
 			for( int j=0; j < mapSize; j++ ) {
 				map[i][j] = Tile.EMPTY;
@@ -68,7 +95,10 @@ public class DungeonScene implements Renderer3D {
 		map[24][10] = Tile.CORRIDOR;
 		map[24][11] = Tile.CORRIDOR;
 		map[24][13] = Tile.CORRIDOR;
-		map[24][14] = Tile.CORRIDOR;
+		map[24][14] = Tile.CORRIDOR;*/
+		
+		
+		
 		// place walls in
 		for( int i=0; i < mapSize; i++ ) {
 			for( int j=0; j < mapSize; j++ ) {
@@ -107,17 +137,47 @@ public class DungeonScene implements Renderer3D {
 		texFloor = app3D.getResources().loadTexture("res/textures/brick.png", "texFloor");
 		texWall  = app3D.getResources().loadTexture("res/textures/wall.png", "texWall");
 		texDungeon = app3D.getResources().loadTexture("res/textures/dungeon.png", "texDungeon");
+		texDetail  = app3D.getResources().loadTexture("res/textures/details.png", "texDetails");
 		
-		archModel    = ModelImporter.importModel("res/models/Arch.gmmod");
-		wallModel    = ModelImporter.importModel("res/models/Wall.gmmod");
-		floorModel   = ModelImporter.importModel("res/models/Floor.gmmod");
-		ceilingModel = ModelImporter.importModel("res/models/Ceiling.gmmod");
+		archModel    	= ModelImporter.importModel("res/models/Arch.gmmod");
+		wallModel    	= ModelImporter.importModel("res/models/Wall.gmmod");
+		floorModel   	= ModelImporter.importModel("res/models/Floor.gmmod");
+		ceilingModel 	= ModelImporter.importModel("res/models/Ceiling.gmmod");
+		candelabraModel = ModelImporter.importModel("res/models/Candelabra.gmmod");
+		torch1Model     = ModelImporter.importModel("res/models/Torch1.gmmod");
 	
 		// Generate block:
 		//block = ModelImporter.importModel("res/models/block.gmmod");
 		sceneFloors = generateScene_Floors( map );
 		sceneWalls  = generateScene_Walls( map );
-	
+		sceneDetails = generateScene_Details( map );
+		
+		//***************************************************************** //
+		// Load Entity resources
+		VertexBuffer entityModel = ModelImporter.importModel("res/models/Enemy.gmmod");
+		
+		
+		//***************************************************************** //
+		// Spawn in Entities
+		int maxEntities = 30;
+		for( int i=0; i < mapSize && maxEntities > 0; i++ ) {
+			for( int j=0; j < mapSize && maxEntities > 0; j++ ) {
+				if( map[i][j] == Tile.ROOM ) {
+					if( generator.nextInt(10) == 1 ) {
+						// Spawn entity
+						EntityCardPic ecp = new EntityCardPic(texWall, entityModel, (i+0.5f)*scale, (j+0.5f)*scale, 5, 10, d, scale);
+						entities.add( ecp );
+						maxEntities --;
+						//System.out.println("Entity spawned!");
+					}
+				}
+			}
+		}
+		// **************************************************************** //
+		
+		
+		playerLight = new PointLight(0.0f, 0.0f, 0.0f, 128.0f, 255.0f, 255.0f, 255.0f, 1.0f);
+		lights.add(playerLight);
 	}
 
 	@Override
@@ -126,6 +186,29 @@ public class DungeonScene implements Renderer3D {
 		// Movement and mouse look
 		mouseLook();
 		movement();
+
+		
+		
+		// Lights
+		for( PointLight pl : lights) pl.update();
+		
+		final Camera cam = app3D.getCamera();
+		playerLight.x = cam.getX();
+		playerLight.y = cam.getY();
+		playerLight.z = cam.getZ();
+		
+		// Entities
+		for( Entity e : entities ) e.update();
+		
+		
+		/*if( Keyboard.isKeyDown(Keyboard.KEY_SPACE) && !placed) {
+			PointLight pl = new PointLight(cam.getX(), cam.getY(), cam.getZ(), 64.0f, 220.0f, 180.0f, 25.0f, 1.0f);
+			lights.add(pl);
+			placed = true;
+		}else
+		if( !Keyboard.isKeyDown(Keyboard.KEY_SPACE)){
+			placed = false;
+		}*/
 		
 		
 		
@@ -139,9 +222,26 @@ public class DungeonScene implements Renderer3D {
 	public void render3D() {
 		// ****************** //
 		// Shader properties
-		Camera cam = app3D.getCamera();
-		for( int i=0; i < 8; i++ ){
-			app3D.getActiveShader().setLight(i, cam.getX(), cam.getY(), cam.getZ(), 256.0f);
+		final Camera cam = app3D.getCamera();
+		float cx, cy, cz;
+		cx = cam.getX();
+		cy = cam.getY();
+		cz = cam.getZ();
+		Comparator<PointLight> c = new Comparator<PointLight>() {
+			
+			@Override
+			public int compare(PointLight o1, PointLight o2) {
+				
+				float l1 = (float) Math.sqrt( (o1.x-cx)*(o1.x-cx) + (o1.y-cy)*(o1.y-cy) + (o1.z-cz)*(o1.z-cz));
+				float l2 = (float) Math.sqrt( (o2.x-cx)*(o2.x-cx) + (o2.y-cy)*(o2.y-cy) + (o2.z-cz)*(o2.z-cz));
+				return (l1 < l2)?-1:1;
+			}
+		};
+		lights.sort(c);
+		for( int i=0; i < Math.min(16, lights.size()); i++ ){
+
+			PointLight e = lights.get(i);
+			app3D.getActiveShader().setLight(i, e.x, e.y, e.z, e.range);
 		}
 		
 		app3D.getActiveShader().setUniformLighting();
@@ -181,6 +281,14 @@ public class DungeonScene implements Renderer3D {
 		// Draw walls
 		//texWall.bind();
 		sceneWalls.render();
+		
+		// Draw details
+		texDetail.bind();
+		sceneDetails.render();
+		
+		// Entities
+		for( Entity e : entities ) e.render();
+		
 	}
 
 	@Override
@@ -232,6 +340,8 @@ public class DungeonScene implements Renderer3D {
 	private void movement(){
 		Camera cam = app3D.getCamera();
 		
+		cam.wobble += 0.05;
+		
 		Vector3f position, direction;
 		position  = cam.getPosition();
 		direction = cam.getDirection();
@@ -240,9 +350,10 @@ public class DungeonScene implements Renderer3D {
 		yaw   = cam.getYaw();
 		speed = 2.0f;
 		
+		
 		direction.set((float) Math.cos(yaw + Math.PI / 2.0) * speed, (float) Math.sin(yaw + Math.PI / 2.0) * speed, 0);
 		
-		
+		boolean pressed = false;
 		// Movement controls
 		if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
 			if( map_free(position.x - direction.x - playerPadding*sign(direction.x), position.y )){
@@ -251,6 +362,7 @@ public class DungeonScene implements Renderer3D {
 			if( map_free(position.x , position.y - direction.y - playerPadding*sign(direction.y) )){
 				position.y -= direction.y;
 			}
+			pressed = true;
 			//Vector3f.sub(position, direction, position);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
@@ -260,6 +372,7 @@ public class DungeonScene implements Renderer3D {
 			if( map_free(position.x , position.y + direction.y + playerPadding*sign(direction.y) )){
 				position.y += direction.y;
 			}
+			pressed = true;
 			//Vector3f.add(position, direction, position);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
@@ -271,7 +384,7 @@ public class DungeonScene implements Renderer3D {
 			if( map_free(position.x , position.y - direction.y - playerPadding*sign(direction.y) )){
 				position.y -= direction.y;
 			}
-			
+			pressed = true;
 			//direction.set((float) Math.cos(yaw) * speed, (float) Math.sin(yaw) * speed, 0);
 			//Vector3f.sub(position, direction, position);
 		}
@@ -283,8 +396,13 @@ public class DungeonScene implements Renderer3D {
 			if( map_free(position.x , position.y + direction.y + playerPadding*sign(direction.y) )){
 				position.y += direction.y;
 			}
+			pressed = true;
 			//direction.set((float) Math.cos(yaw) * speed, (float) Math.sin(yaw) * speed, 0);
 			//Vector3f.add(position, direction, position);
+		}
+		
+		if( pressed ) {
+			cam.wobble += 0.38*direction.length();
 		}
 		
 		cam.setPosition( position );
@@ -321,13 +439,13 @@ public class DungeonScene implements Renderer3D {
 					right = map[i+1][j];
 					
 					boolean rotate = false;
-					if( left == Tile.CORRIDOR || right == Tile.CORRIDOR && !(Tile.CORRIDOR == up || Tile.CORRIDOR == down) ) {
+					if( (left == Tile.CORRIDOR || left == Tile.ROOM )|| (right == Tile.CORRIDOR || right == Tile.ROOM ) && !(Tile.CORRIDOR == up || Tile.ROOM == up || Tile.CORRIDOR == down || Tile.ROOM == down) ) {
 						rotate = true;
 					}
-					if( left  == Tile.CORRIDOR && up   == Tile.CORRIDOR || 
-						right == Tile.CORRIDOR && up   == Tile.CORRIDOR || 
-						left  == Tile.CORRIDOR && down == Tile.CORRIDOR || 
-						right == Tile.CORRIDOR && down == Tile.CORRIDOR ){
+					if( (left  == Tile.CORRIDOR||left == Tile.ROOM) && (up   == Tile.CORRIDOR|| up == Tile.ROOM) || 
+						(right == Tile.CORRIDOR||right == Tile.ROOM) && (up   == Tile.CORRIDOR|| up == Tile.ROOM) || 
+						(left  == Tile.CORRIDOR||left == Tile.ROOM ) && (down == Tile.CORRIDOR|| up == Tile.ROOM) || 
+						(right == Tile.CORRIDOR||right == Tile.ROOM) && (down == Tile.CORRIDOR|| up == Tile.ROOM) ){
 						corner = true;
 					}
 					if( !corner ) {
@@ -417,7 +535,130 @@ public class DungeonScene implements Renderer3D {
 		return vb;
 	}
 	
+	public VertexBuffer generateScene_Details( Tile[][] map ) {
+		VertexBuffer vb = new VertexBuffer();
+		
+		for( int i=1; i < mapSize-1; i++ ) {
+			for( int j=1; j < mapSize-1; j++ ) {
+				if( map[i][j] == Tile.ROOM ) {
+					Tile upt, downt, leftt, rightt;
+					upt    = map[i][j-1];
+					downt  = map[i][j+1];
+					leftt  = map[i-1][j];
+					rightt = map[i+1][j];
+					
+					boolean up, down, left, right;
+					up    = (upt == Tile.CORRIDOR || upt == Tile.ROOM );
+					down  = (downt == Tile.CORRIDOR || downt == Tile.ROOM );
+					left  = (leftt == Tile.CORRIDOR || leftt == Tile.ROOM );
+					right = (rightt == Tile.CORRIDOR || rightt == Tile.ROOM );
+					
+					boolean corner = (up && left || up&& right || down && left || down && right) && !(down && up) && !(left&&right);
+					if( corner ) {
+						if ( generator.nextDouble()*100.0 <= 25.0 ){
+							Matrix4f mf = new Matrix4f();
+							mf.setIdentity();
+							
+							mf.translate(new Vector3f(i+0.5f, j+0.5f, 0.0f));
+							mf.rotate((float) -Math.PI/4, new Vector3f( 0.0f, 0.0f, 1.0f));
+							vb.addModel(candelabraModel, mf);
+							mf.rotate((float) ((float) -Math.PI/1.2), new Vector3f( 0.0f, 0.0f, 1.0f));
+							mf.translate(new Vector3f(0.2f, 0.2f, 0.0f));
+							vb.addModel(candelabraModel, mf);
+							
+							PointLight pl = new PointLightFlickering((i+0.5f)*scale, (j+0.5f)*scale, 16.0f, 32.0f, 240.0f, 120.0f, 60.0f, 1.0f);
+							lights.add(pl);
+						}
+					}
+				}else if( map[i][j] == Tile.CORRIDOR ) {
+					
+					Tile upt, downt, leftt, rightt;
+					upt    = map[i][j-1];
+					downt  = map[i][j+1];
+					leftt  = map[i-1][j];
+					rightt = map[i+1][j];
+					
+					boolean[] a = new boolean[4];
+					a[0] = (upt == Tile.WALL );
+					a[1] = (downt == Tile.WALL );
+					a[2] = (leftt == Tile.WALL );
+					a[3] = (rightt == Tile.WALL );
+					
+					boolean[] b = new boolean[4];
+					b[0] = (upt == Tile.ROOM );
+					b[1] = (downt == Tile.ROOM );
+					b[2] = (leftt == Tile.ROOM );
+					b[3] = (rightt == Tile.ROOM );
+					
+					ArrayList<Boolean> al = new ArrayList<>();
+					ArrayList<Integer> ai = new ArrayList<>();
+					for( int x=0; x < 4; x++ ) {
+						if( a[x]){
+							al.add(a[x]);
+							ai.add(x);
+						}
+						if( b[x]){
+							al.clear();
+							ai.clear();
+							x = 4;
+						}
+					}
+					
+					int count = ai.size();
+					if( count > 0 ) {
+						if ( generator.nextDouble()*100.0 <= 25.0 ){
+							Matrix4f mf = new Matrix4f();
+							mf.setIdentity();
+							mf.translate(new Vector3f(i, j+1, 0.5f));
+							int lp = 0;
+							if( count > 1 ) {
+								lp = generator.nextInt(count-1);
+							} else {
+								lp = 0;
+							}
+							switch( ai.get(lp) ) {
+							
+								// up:
+								case 0:
+									//mf.rotate((float) ((float) Math.PI), new Vector3f( 0.0f, 0.0f, 1.0f));
+								break;
+								
+								// down
+								case 1:
+									mf.rotate((float) ((float) -Math.PI), new Vector3f( 0.0f, 0.0f, 1.0f));
+									mf.translate(new Vector3f(0.0f, 0.0f, 0.0f));
+								break;
+								
+								// left:
+								case 2:
+									mf.rotate((float) ((float) -Math.PI/2), new Vector3f( 0.0f, 0.0f, 1.0f));
+									mf.translate(new Vector3f(0.0f, 1.0f, 0.0f));
+								break;
+								
+								// right:
+								case 3:
+									mf.rotate((float) ((float) Math.PI/2), new Vector3f( 0.0f, 0.0f, 1.0f));
+									
+								break;
+							}
+							vb.addModel(torch1Model, mf);
+							PointLight pl = new PointLightFlickering((i)*scale, (j+1)*scale, 16.0f, 32.0f, 240.0f, 120.0f, 60.0f, 1.0f);
+							lights.add(pl);
+						}
+					}
+				}
+			}
+		}
+		
+		vb.prepareNormals();
+		vb.freeze();
+		return vb;
+	}
+	
 	public boolean map_free( float x, float y ) {
+		if( Keyboard.isKeyDown(Keyboard.KEY_C)){
+			return true;
+		}
 		int gX, gY;
 		gX = (int)Math.floor(x/scale);
 		gY = (int)Math.floor(y/scale);
